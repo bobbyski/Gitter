53f007407bb4f7b4b85dce9c5ff7f51ba706350c
Bobby Skinner <bobbyski@mac.com>
2026-03-20 21:59:40 -0400

    Started project window

--- ProjectView.py
+++ ProjectView.py
@@ -1,13 +1,39 @@
 from textual.app import App, ComposeResult
-from textual.widgets import Static
+from textual.widgets import Static, Label
+from model.Project import Project
+
 
 class ProjectView(Static):
-    """A widget that presents a black view with a double white border."""
-    
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.title = "Project View"
+        self.projects = []
+        
+        self.setupSampleData()
+
+    def compose(self) -> ComposeResult:
+        yield Label("Project View")
+
+
+    def on_mount(self) -> None:
+        self.border_title = self.title
+
+    def setupSampleData(self):
+        self.title = "Bobby's Project"
+        self.projects = [
+            Project(f"Project {i}", f"/path/to/project_{i.lower()}")
+            for i in "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        ]
+        # Adding a few more to reach 30
+        self.projects.extend([
+            Project("Project AA", "/path/to/project_aa"),
+            Project("Project BB", "/path/to/project_bb"),
+            Project("Project CC", "/path/to/project_cc"),
+            Project("Project DD", "/path/to/project_dd")
+        ])
+        
 
 class ProjectApp(App):
-    """A simple Textual app to display ProjectView."""
-    
     CSS_PATH = "project_view.tcss"
     
     def compose(self) -> ComposeResult:

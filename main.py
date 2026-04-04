7e1e21443bcfed816a80c198223398f0bd263c12
Bobby Skinner <bobbyski@mac.com>
2026-04-04 13:25:28 -0400

    GIT-6 Add argparse to main

--- main.py
+++ main.py
@@ -1,4 +1,20 @@
+#!/usr/bin/env python3
+
 import argparse
+from model.MainFileManager import MainFileManager
+from pathlib import Path
+from rich.console import Console
+from rich.table import Table
+
+def build_parser():
+    parser = argparse.ArgumentParser(description='Git Repository Manager')
+
+    subparsers = parser.add_subparsers(title='commands', dest='command', required=True)
+    subparsers.add_parser('status', help='Show status of all projects')
+    subparsers.add_parser('tui', help='Open TUI viewer')
+    subparsers.add_parser('version', help='show version information')
+
+    return parser
 
 def show_version(version):
     """Display the version information and exit."""
@@ -6,16 +22,44 @@ def show_version(version):
     exit(0)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def status():
+    table = Table(title="Projects")
+    table.add_column("Name", style="bold cyan", no_wrap=True)
+    table.add_column("Directory", style="dim")
+    table.add_column("Status")
+    table.add_column("Release")
+    table.add_column("Issues in Current/Next Release")
 
+    for project in MainFileManager.shared.projects:
+        table.add_row(
+            project.name,
+            project.directory,
+            str(project.status),
+            project.current_release(),
+            project.issues_string_for_release(),
+        )
 
-# cli.add_command(show_version)
+    Console().print(table)
 
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
-    show_version('0.0.1')
+    parser = build_parser()
+
+    # print(parser.parse_args())
+
+    if parser.parse_args().command == 'tui':
+        from MenuApp import MenuApp
+        app = MenuApp()
+        app.run()
+    elif parser.parse_args().command == 'version':
+        show_version('0.0.8')
+    else:
+        pathname = str(Path.home() / ".gitter")
+        MainFileManager.load_shared_from_json(pathname)
+
+        for project in MainFileManager.shared.projects:
+            project.update()
+
+        if parser.parse_args().command == 'status':
+            status()
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/

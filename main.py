31ed3e58b1bdf1baff58aec334643f36f063989f
Bobby Skinner <bobbyski@mac.com>
2026-04-13 22:28:32 -0400

    feat(GIT-47): command line refactor
    Notes

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import argparse
 from rich.markdown import Markdown
 
 from CommandLine.CommandLineIssue import issues
+from CommandLine.CommandLineNotes import notes
 from CommandLine.CommandLineStatus import status
 from model.MainFileManager import MainFileManager
 from model.Project import Project
@@ -77,26 +78,6 @@ def add_project(name=None):
     MainFileManager.save_shared_to_json(str(Path.home() / ".gitter"))
     Console().print(f"[green]Added:[/green] {project_name} ({cwd})")
 
-
-def notes(project_name=None, release_name=None, markdown=False, raw=False, code_theme='monokai' ):
-    console = Console()
-
-    for project in MainFileManager.shared.projects:
-        if project_name is not None and project.name.lower() != project_name.lower():
-            continue
-
-        releases_with_issues = [r for r in project.releases if len(r.issues) > 0]
-        if not releases_with_issues:
-            continue
-
-        if raw:
-            text = project.release_notes_markdown( release_name=release_name )
-            print(text)
-        elif markdown:
-            markdown = Markdown(project.release_notes_markdown( release_name=release_name ), code_theme=code_theme)
-            console.print(markdown)
-        else:
-            console.print(project.release_notes( release_name=release_name ))
 
 def main():
     parser = build_parser()

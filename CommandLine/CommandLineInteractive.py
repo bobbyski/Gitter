fd2bbcee89d863e972f9529a6e0d85fe1592654d
Bobby Skinner <bobbyski@mac.com>
2026-04-14 18:35:32 -0400

    Computer transition

--- CommandLine/CommandLineInteractive.py
+++ CommandLine/CommandLineInteractive.py
@@ -1,23 +1,24 @@
 from inquirer_textual import prompts
+
+from BusinessLogic.toml_helper import TomlHelper
 from TUI.MenuApp import MenuApp
 
 def interactive():
     commands = ["add", "status", "issues", "notes", "raw", "version", "help", "tui", "exit"]
     choice = prompts.select("What would you like to do?", choices=commands)
 
-    if choice == "exit":
+    if choice.value == "exit":
         return
-    elif choice.lower() == "tui":
+    elif choice.value.lower() == "tui":
         app = MenuApp()
         app.run()
-    elif choice.lower() == "help":
+    elif choice.value.lower() == "help":
         commands = ["add", "status", "issues", "notes", "raw", "version", "help", "tui", "exit"]
         cmd = prompts.select("What would you like to do?", choices=commands)
         help(cmd)
-    elif choice.lower() == "version":
-        from main import __version__
-        print(f"Version: {__version__}")
-    elif choice.lower() == "issues":
+    elif choice.value.lower() == "version":
+        print(f"Version: {TomlHelper().get_version()}")
+    elif choice.value.lower() == "issues":
         from main import issues
         issues()
     else:
